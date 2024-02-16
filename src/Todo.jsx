b54cbc1084b31d33cd858de63d17c0ccdb813bd6
saifulmahbub <saifulislammahbub83@gmail.com>
2024-02-16 08:24:07 +0100

    rendering conditional

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,6 +1,11 @@
 export default function Todo({ task, isDone }) {
-  if (isDone) {
-    return <li>finished :{task}</li>;
-  }
-  return <li>work on:{task}</li>;
+  // if (isDone) {
+  //   return <li>finished :{task}</li>;
+  // }
+  // return <li>work on:{task}</li>;
+  return (
+    <li>
+      {isDone ? "finished" : "work on"}: {task}
+    </li>
+  );
 }
